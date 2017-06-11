7b55c83939d639d224cb487f1d26fc7f68434e16
radeshshiwlochan <radesh0430@gmail.com>
2017-06-11 16:19:18 -0400

    completes all comments of the program

--- main.cpp
+++ main.cpp
@@ -33,7 +33,10 @@ public:
 };
 
 CheckTheCheck::CheckTheCheck() {
-	//initializes the chessBoard to empty spaces( '.' )
+	/**
+		initializes the chessBoard to empty spaces( '.'  represent
+		empty spaces)
+	*/	
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			chessBoard[row][col] = '.';
@@ -42,25 +45,40 @@ CheckTheCheck::CheckTheCheck() {
 }
 
 void CheckTheCheck::exeCheckTheCheck(string inputFile, string outputFile) {
-
+	/**
+		-reads the input from inputFile
+		-print output to outputFile
+		loads the input into chessBoard using the while loop. Then 
+		endOfInput() function checks if the loaded input is all empty
+		spaces which then breaks the while loop. printCount prints the
+		current input number and then all functions are called to 
+		determine if a chess piece checks a king of either color.
+	*/
+	//reads input from inputFile
 	ifstream readFile;
 	readFile.open(inputFile);
+	//prints output to outputFile
 	ofstream printToFile;
 	printToFile.open(outputFile);
+	//char used to read in values from inputFile
 	char boardConfig = 'x';
+	//count used to keep track of the input number
 	int count = 0;
+	//while loop runs until end of file or end of input ( 64 '.' read)
 	while(readFile.good()) {
 		count++;
 		for(int row = 0; row < 8; ++row) {
 			for(int col = 0; col < 8; ++col) {
 				readFile >> boardConfig;
 				chessBoard[row][col] = boardConfig;
-			}
-		}
+			}//for col
+		}//for row
+		//checks if the input is all '.' in which case breaks the loop
 		if(endOfInput())
 			break;
-
+		//print the current Game number to file
 		printCount(printToFile, count); 
+		//call functions to check if respective pieces have king in check
 		if(rookCheck(printToFile)         ||
 		   bishopCheck(printToFile)       ||
 		   queenCheck(printToFile)        ||
@@ -69,30 +87,40 @@ void CheckTheCheck::exeCheckTheCheck(string inputFile, string outputFile) {
 		   blackPawnCheck(printToFile)    ||
 		   whitePawnCheck(printToFile)      )
 			cout << "";
-
+		//if either color king is not in check, display as such
 		else  
 			printToFile << "no king is in check.\n";
 		
 	}
+	//close files
 	readFile.close();
 	printToFile.close();	 
-}
+}//end of exeCheckTheCheck
 
 bool CheckTheCheck::endOfInput() {
-
+	/**
+		uses a flag to check if all empty spaces are read in from
+		input which signifies that the input is reached. If all 
+		empty spaces are read in, returns true, else returns false
+	*/
 	bool flag = true;
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			if(chessBoard[row][col] != '.')
 				flag = false;
-		}
-	}
+		}//for col
+	}// for row
 	return flag;
-}
+}//end of endOfInput
 
 bool CheckTheCheck::advRowLftToRght(int rowPos, int colPos, 
 														char chessPiece) {
-
+	/**
+		iterates through the current row until colPos == 6 or until a 
+		chessPiece is reached. Then checks if the next position (column)
+		 == chessPiece which will be k or K. Used by rookCheck and
+		 queenCheck
+	*/
 	while(colPos + 1 < 7 && chessBoard[rowPos][colPos + 1] == '.')
 		colPos++;
 	return chessBoard[rowPos][colPos + 1] == chessPiece;
@@ -100,7 +128,12 @@ bool CheckTheCheck::advRowLftToRght(int rowPos, int colPos,
 
 bool CheckTheCheck::advRowRghtToLft(int rowPos, int colPos, 
 														char chessPiece) {
-
+	/**
+		iterates through the current row until colPos == 1 or until a 
+		chessPiece is reached. Then checks if the next position (column)
+		== chessPiece which will be k or K. Used by rookCheck and
+		 queenCheck
+	*/
 	while(colPos - 1 > 0 && chessBoard[rowPos][colPos - 1] == '.')
 		colPos--;
 	return chessBoard[rowPos][colPos - 1] == chessPiece;
@@ -109,6 +142,12 @@ bool CheckTheCheck::advRowRghtToLft(int rowPos, int colPos,
 bool CheckTheCheck::advColDownToTop(int rowPos, int colPos, 
 														char chessPiece) {
 
+	/**
+		iterates through the current col until rowPos == 0 or until a 
+		chessPiece is reached. Then checks if the next position (row)
+		 == chessPiece which will be k or K. Used by rookCheck and
+		 queenCheck
+	*/
 	while(rowPos - 1 > 0 && chessBoard[rowPos - 1][colPos] == '.')
 		rowPos--;
 	return chessBoard[rowPos - 1][colPos] == chessPiece;
@@ -117,6 +156,12 @@ bool CheckTheCheck::advColDownToTop(int rowPos, int colPos,
 bool CheckTheCheck::advColTopToDown(int rowPos, int colPos, 
 														char chessPiece) {
 
+	/**
+		iterates through the current col until rowPos == 6 or until a 
+		chessPiece is reached. Then checks if the next position (row)
+		 == chessPiece which will be k or K. Used by rookCheck and
+		 queenCheck
+	*/
 	while(rowPos + 1 < 8 && chessBoard[rowPos + 1][colPos] == '.')
 		rowPos++;
 	return chessBoard[rowPos + 1][colPos] == chessPiece;
@@ -124,7 +169,14 @@ bool CheckTheCheck::advColTopToDown(int rowPos, int colPos,
 
 bool CheckTheCheck::advDiagDownTopLftRght(int rowPos, int colPos, 
 														char chessPiece) {
-
+	/**
+		iterates through the chessBoard in a diagonal going to the upper
+		right corner of the board. Checks that the rowPos and colPos are
+		within bounds and that the next spot is not occupied by another
+		chess piece. When while loop is false it checks the next position
+		if it is == chessPiece which will be k or K.Used by bishopCheck and
+		queenCheck
+	*/
 	while(rowPos - 1 > 0 && colPos + 1 < 7 && 
 		             chessBoard[rowPos - 1][colPos + 1] == '.' ) {
 		rowPos--;
@@ -135,7 +187,14 @@ bool CheckTheCheck::advDiagDownTopLftRght(int rowPos, int colPos,
 
 bool CheckTheCheck::advDiagTopDownLftRght(int rowPos, int colPos, 
 														char chessPiece) {
-
+	/**
+		iterates through the chessBoard in a diagonal going to the lower
+		right corner of the board. Checks that the rowPos and colPos are
+		within bounds and that the next spot is not occupied by another
+		chess piece. When while loop is false it checks the next position
+		if it is == chessPiece which will be k or KUsed by bishopCheck and
+		queenCheck
+	*/
 	while(rowPos + 1 < 7 && colPos + 1 < 7 && 
 					chessBoard[rowPos + 1][colPos + 1] == '.') {
 		rowPos++;
@@ -147,6 +206,14 @@ bool CheckTheCheck::advDiagTopDownLftRght(int rowPos, int colPos,
 bool CheckTheCheck::advDiagDownTopRghtLft(int rowPos, int colPos, 
 														char chessPiece) {
 
+	/**
+		iterates through the chessBoard in a diagonal going to the upper
+		left corner of the board. Checks that the rowPos and colPos are
+		within bounds and that the next spot is not occupied by another
+		chess piece. When while loop is false it checks the next position
+		if it is == chessPiece which will be k or K.Used by bishopCheck and
+		queenCheck
+	*/
 	while(rowPos - 1 > 0 && colPos - 1 > 0 &&
 						chessBoard[rowPos - 1][colPos - 1] == '.') {
 		rowPos--;
@@ -158,6 +225,14 @@ bool CheckTheCheck::advDiagDownTopRghtLft(int rowPos, int colPos,
 bool CheckTheCheck::advDiagTopDownRghtLft(int rowPos, int colPos, 
 														char chessPiece) {
 
+	/**
+		iterates through the chessBoard in a diagonal going to the lower
+		left corner of the board. Checks that the rowPos and colPos are
+		within bounds and that the next spot is not occupied by another
+		chess piece. When while loop is false it checks the next position
+		if it is == chessPiece which will be k or K. Used by bishopCheck and
+		queenCheck
+	*/
 	while(rowPos + 1 < 7 && colPos - 1 > 0 && chessBoard[rowPos + 1][colPos - 1]) {
 		rowPos++;
 		colPos--;
@@ -167,6 +242,11 @@ bool CheckTheCheck::advDiagTopDownRghtLft(int rowPos, int colPos,
 
 bool CheckTheCheck::advOnePosAllDir(int rowPos, int colPos,
 													char chessPiece) {
+
+	/**
+		iterates one position in all directions to check if the spot next
+		to the current position == checkPiece. Used by kingCheck
+	*/
 
 	if((rowPos - 1 >= 0 && chessBoard[rowPos - 1][colPos] == chessPiece) ||
 	   (colPos - 1 >= 0 && chessBoard[rowPos][colPos - 1] == chessPiece) ||
@@ -187,6 +267,13 @@ bool CheckTheCheck::advOnePosAllDir(int rowPos, int colPos,
 bool CheckTheCheck::longLShapeDir(int rowPos, int colPos, 
 												  char chessPiece) {
 	
+	/**
+		Used by knighCheck to move in a 'L' shape meaning two spaces in one
+		direction and one direction at the end of the two space advancement.
+		The if statement is used to check if that if the knight moves from its
+		current position, will it check a king. It checks that the colPos and 
+		rowPos are within bounds and if a king can be taked from current position. 
+	*/
 	if((colPos + 2 <= 7 && colPos + 2 >= 0 && rowPos + 1 <= 7 && 
 	    rowPos + 1 >= 0 && chessBoard[rowPos + 1][colPos + 2] == chessPiece)  || 
 	   (colPos + 2 <= 7 && colPos + 2 >= 0 && rowPos - 1 <= 7 && 
@@ -210,6 +297,13 @@ bool CheckTheCheck::longLShapeDir(int rowPos, int colPos,
 bool CheckTheCheck::shortLShapeDir(int rowPos, int colPos,
 												   char chessPiece) {
 
+	/**
+		Used by knighCheck to move in a 'L' shape meaning one space in one
+		direction and two spaces in another direction at the end of the two space 
+		advancement.The if statement is used to check if that if the knight moves from 
+		its current position, will it check a king. It checks that the colPos and 
+		rowPos are within bounds and if a king can be taked from current position. 
+	*/
 	if((colPos + 1 <= 7 && colPos + 1 >= 0 && rowPos + 2 <= 7 &&
 		rowPos + 2 >= 0 && chessBoard[rowPos + 2][colPos + 1] == chessPiece)  || 
 	   (colPos - 1 <= 7 && colPos - 1 >= 0 && rowPos + 2 <= 7 &&
@@ -229,10 +323,17 @@ bool CheckTheCheck::shortLShapeDir(int rowPos, int colPos,
 	   	return true;
 	return false;   	
 
-} 
+}
 
 bool CheckTheCheck::rookCheck(ofstream& printToFile) {
 
+	/**
+		used to check if a rook chess piece has a king in check. The first
+		if statement checks for a white rook and uses the four functions to
+		check if a black king can be taken from the current position of the rook.
+		The second if statement does the same thing as the first except that it 
+		checks for a black rook and a white king
+	*/
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			if(chessBoard[row][col] == 'R') {
@@ -260,6 +361,13 @@ bool CheckTheCheck::rookCheck(ofstream& printToFile) {
 
 bool CheckTheCheck::bishopCheck(ofstream& printToFile) {
 
+	/**
+		used to check if a bishop chess piece has a king in check. The first
+		if statement checks for a white bishop and uses the four functions to
+		check if a black king can be taken from the current position of the bishop.
+		The second if statement does the same thing as the first except that it 
+		checks for a black bishop and a white king
+	*/
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			if(chessBoard[row][col] == 'B') {
@@ -287,6 +395,15 @@ bool CheckTheCheck::bishopCheck(ofstream& printToFile) {
 
 bool CheckTheCheck::knightCheck(ofstream& printToFile) {
 
+	/**
+		used to check if a knight chess piece has a king in check. The first if 
+		statement checks for a white knight and uses the longLShapeDir and 
+		shortLShapeDir functions to check if a black king can be taken from the 
+		current position of the rook. A knight can move in 16 directions from its
+		current positon, hence the need of two functions to check all the possible 
+		moves. The second if statement does the same thing as the first except that 
+		it checks for a black knight and a white king
+	*/
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			if(chessBoard[row][col] == 'N' && longLShapeDir(row, col, 'k') &&
@@ -306,7 +423,13 @@ bool CheckTheCheck::knightCheck(ofstream& printToFile) {
 }
 
 bool CheckTheCheck::kingCheck(ofstream& printToFile) {
-
+	/**
+		used to check if a king chess piece has a king of the opposite color 
+		in check. The first if statement checks for a white king and uses the 
+		function to check if a black king can be taken from the current position
+		 of the king. The second if statement does the same thing as the first 
+		 except that it checks for a black king and a white king
+	*/
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			if(chessBoard[row][col] == 'K' && 
@@ -349,9 +472,9 @@ bool CheckTheCheck::queenCheck(ofstream& printToFile) {
 					{
 						printToFile << "black king is in check.\n";
 						return true;
-					} 
+					} //if
 					
-            }
+            }//if
 			if(chessBoard[row][col] == 'q')                 {
 				if(advColDownToTop(row,  col, 'K'      ) ||
 				   advColTopToDown(row,  col, 'K'      ) ||
