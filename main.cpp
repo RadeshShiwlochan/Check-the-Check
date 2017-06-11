9acaf9d5b5a34f3ccedfcc33ae1af2e77e86e890
radeshshiwlochan <radesh0430@gmail.com>
2017-06-11 14:14:35 -0400

    adds comments for some of the functions

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ private:
 public:
 	CheckTheCheck();
 	void exeCheckTheCheck(string, string);
-	bool checkEndOfInput();	
+	bool endOfInput();	
 	bool advRowLftToRght(int, int, char);
 	bool advRowRghtToLft(int, int, char);
 	bool advColDownToTop(int, int, char);
@@ -33,7 +33,7 @@ public:
 };
 
 CheckTheCheck::CheckTheCheck() {
-
+	//initializes the chessBoard to empty spaces( '.' )
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
 			chessBoard[row][col] = '.';
@@ -57,8 +57,9 @@ void CheckTheCheck::exeCheckTheCheck(string inputFile, string outputFile) {
 				chessBoard[row][col] = boardConfig;
 			}
 		}
-		if(checkEndOfInput())
+		if(endOfInput())
 			break;
+
 		printCount(printToFile, count); 
 		if(rookCheck(printToFile)         ||
 		   bishopCheck(printToFile)       ||
@@ -77,7 +78,7 @@ void CheckTheCheck::exeCheckTheCheck(string inputFile, string outputFile) {
 	printToFile.close();	 
 }
 
-bool CheckTheCheck::checkEndOfInput() {
+bool CheckTheCheck::endOfInput() {
 
 	bool flag = true;
 	for(int row = 0; row < 8; ++row) {
@@ -324,14 +325,23 @@ bool CheckTheCheck::kingCheck(ofstream& printToFile) {
 }
 
 bool CheckTheCheck::queenCheck(ofstream& printToFile) {
+	/**
+		reads the chessboard and searches for a queen. 
+		The first if statement checks solely for Q (white queen),
+		then calls methods to check if k (black king) can be
+		taken from the queen's current position. The second 
+		if statement does the same as the first except it 
+		checks for q (black queen) and check if K (white king)
+		can be taken from current posit
+	*/
 
 	for(int row = 0; row < 8; ++row) {
 		for(int col = 0; col < 8; ++col) {
-			if(chessBoard[row][col] == 'Q'  )                {     
-				if(advColDownToTop(row,  col,'k'      )  ||
-					advColTopToDown(row, col,'k'      )  ||
-					advRowLftToRght(row, col,'k'      )  ||
-					advRowRghtToLft(row, col,'k'      )  ||
+			if(chessBoard[row][col] == 'Q')                {     
+				if(advColDownToTop(row,  col,'k'       ) ||
+					advColTopToDown(row, col,'k'       ) ||
+					advRowLftToRght(row, col,'k'       ) ||
+					advRowRghtToLft(row, col,'k'       ) ||
 					advDiagDownTopLftRght(row, col, 'k') ||
 					advDiagTopDownLftRght(row, col, 'k') ||
 					advDiagDownTopRghtLft(row, col, 'k') ||
@@ -353,31 +363,48 @@ bool CheckTheCheck::queenCheck(ofstream& printToFile) {
 					advDiagTopDownRghtLft(row, col, 'K')    ) {
 					printToFile << "white king is in check.\n";
 					return true;
-			    }
-		    }
-		}
-	}
+			    }// end of if with method calls
+		    }// end of if with searching for q
+		}// for col
+	}//for row
 	return false;
-}
+}// end of queenCheck
 
 bool CheckTheCheck::whitePawnCheck(ofstream& printToFile) {
+	/**
+		reads the chessBoard from right to left and bottom to top.
+		Since a pawn can take a king if the king is diagonal to 
+		it, check positions row - 1 col + 1 and row + 1, col - 1
+		for k (black king).
+		-takeRwPos: used for row - 1
+		-takeColPosLeft: used for col + 1
+		-take ColPosRight: used for col - 1
+	*/
 
 	int takeRwPos = 0;
 	int takeColPosLeft = 0;
 	int takeColPosRight = 0;
 	for(int row = 7; row > 0; --row) {
 		for(int col = 7; col >= 0; --col) {
+			/**
+				if a white pawn is found, set takeRwPos, takeColPosLeft
+				and takeColPosRight	
+			*/
 			if(chessBoard[row][col] == 'P') {
 				takeRwPos = row - 1;
 				takeColPosLeft = col + 1;
 				takeColPosRight = col - 1;
+				/**
+					checks if takeRwPos, takeColPosLeft and takeColPosRight
+					are in bounds and if any of the diagonals are k
+				*/
 				if((takeColPosLeft  >= 0 && takeColPosLeft  <= 7    &&
 				    chessBoard[takeRwPos][takeColPosLeft]   == 'k') ||
 			   	   (takeColPosRight >= 0 && takeColPosRight <= 7    &&
 			   		chessBoard[takeRwPos][takeColPosRight]  == 'k')    ) {
 						printToFile << "black king is in check.\n";
 						return true;
-				}
+				}// if
 		    }//if value == "P"
 		}//for
 	}//for
@@ -385,43 +412,64 @@ bool CheckTheCheck::whitePawnCheck(ofstream& printToFile) {
 }
 
 bool CheckTheCheck::blackPawnCheck(ofstream& printToFile) {
+	/**
+		reads the chessBoard from left to right and top to bottom.
+		Since a pawn can take a king if the king is diagonal to 
+		it, check positions row + 1 col + 1 and row + 1, col - 1
+		for K (white king).
+		-takeRwPos: used for row + 1
+		-takeColPosLeft: used for col + 1
+		-take ColPosRight: used for col - 1
+	*/
 
 	int takeRwPos = 0;
 	int takeColPosLeft = 0;
 	int takeColPosRight = 0;
 	for(int row = 0; row < 7; ++row) {
 		for(int col = 0; col < 8; ++col) {
+			/**
+				if a black pawn is found, set takeRwPos, takeColPosLeft
+				and takeColPosRight	
+			*/
 			if(chessBoard[row][col] == 'p') {
 				takeRwPos = row + 1;
 				takeColPosLeft = col + 1;
 				takeColPosRight = col - 1;
+				/**
+					checks if takeRwPos, takeColPosLeft and takeColPosRight
+					are in bounds and if any of the diagonals are K
+				*/
 				if((takeColPosLeft  >= 0 && takeColPosLeft  <= 7    &&
 				    chessBoard[takeRwPos][takeColPosLeft]   == 'K') ||
 			   	   (takeColPosRight >= 0 && takeColPosRight <= 7    &&
 			   		chessBoard[takeRwPos][takeColPosRight]  == 'K')    ) {
 						printToFile << "white king is in check.\n";
 						return true;
-				}
+				}//if
 		    }//if value == "P"
 		}//for
 	}//for
 	return false;
-}
+}//end of blackPawnCheck
 
 void CheckTheCheck::printCount(ofstream& printToFile, int count) {
-    
+    /**
+    	prints the Game count before the individual functions are
+    	called to check the board.
+    */
     printToFile << "Game #" << count <<": ";
-}
+}//end of printCount
 
 int main(int argc, char* argv[]) {
 
+	//check to ensure 2 files are provided, input and output.
 	if(argc != 3) {
-		cout << "Program needs 2 input: an input " <<
-		" file with the board configurations and " <<
+		cout << "Program needs 2 inputs: an input" <<
+		" file with the board configurations and"  <<
 		" an output file, Terminating!\n";
 		return 0;
 	}
 	CheckTheCheck checkTheCheck;
 	checkTheCheck.exeCheckTheCheck(argv[1], argv[2]);
 	return 0;
-}
+}//end of main
